17df9e800b3f2f0e4cf4dfb97f3890ce5f4bf2a0
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-20 13:02:50 +0700

    feat:complete Ex

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,40 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+const buttonStyle = css`
+  wideth: 171.19;
+  height: 50px;
+  padding: 0px 16px;
+  border-radius: 4px;
+  color: white;
+  gap: 8px;
+  margin: 10px;
+`;
+
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <button
+        css={css`
+          ${buttonStyle}
+          background-color: #074ee8;
+        `}
+      >
+        Button
+      </button>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <button
+        css={css`
+          ${buttonStyle}
+          background-color: #07a4e8;
+        `}
+      >
+        Button
+      </button>
+    );
+  }
+}
+
+export default Button;
